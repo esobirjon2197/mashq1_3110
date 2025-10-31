4d323689d484084c148fe3b424c1a0e8a3fca235
esobirjon2197 <esobirjon2197@gmail.com>
2025-10-31 17:39:10 +0500

    Add multiple loops and input handling in main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+# 1
+for i in range(1, 11):
+    print(i)
+
+# 2
+for i in range(10,0,-1):
+    print(i)
+    i += 1
+
+
+# 3
+n = int(input("Son kirit: "))
+
+for i in range(2, n, 2):
+    print(i)
+    i += 1
+
+
+# 4
+n = int(input("Son kirit: "))
+
+for i in range(1, n, 2):
+    print(i)
+    i += 1
+
+
+# 5
+sum = 0
+
+n = int(input("Son kirit: "))
+
+for i in range(1, n):
+     sum += i
+     i += 1
+print(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
